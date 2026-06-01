96e6e90f57279d08e1bb15eb8e20ce4e1445d4c8
deveshgupta01 <deveshg646@gmail.com>
2026-06-01 22:26:45 +0530

    Update two_sum.cpp

--- Week_1/Day_1/two_sum.cpp
+++ Week_1/Day_1/two_sum.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>sum;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(target==(nums[i]+nums[j])){
+                   sum.push_back(i);
+                   sum.push_back(j);
+                }
+            }
+        }
+        return sum;
+    }
+    
+};
