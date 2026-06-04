08550ec4b30f368b0055754c5717623f7624a1fc
deveshgupta01 <deveshg646@gmail.com>
2026-06-04 23:39:31 +0530

    Update Reshape_the_Matrix.cpp

--- Week_1/Day_4/Reshape_the_Matrix.cpp
+++ Week_1/Day_4/Reshape_the_Matrix.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> v(r, vector<int>(c));
+        if(m*n!=r*c){
+            return mat;
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int idx=n*i+j;
+                v[idx/c][idx%c]=mat[i][j];
+            }
+        }
+        return v;
+    }
+};
