a4b7b0568299d43b1fe49ce50081453af41941fe
deveshgupta01 <deveshg646@gmail.com>
2026-06-05 22:59:54 +0530

    Update Valid_Paranthesis.cpp

--- Week_1/Day_5/Valid_Paranthesis.cpp
+++ Week_1/Day_5/Valid_Paranthesis.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+    bool isValid(string s) {
+       int  n=s.size();
+       stack<char>sr;
+       for(int i=0;i<n;i++){
+        char ch=s[i];
+        if(s[i]=='(' || s[i]=='{' ||s[i]=='['){
+            sr.push(ch);
+        }
+        else{
+            if(sr.empty()){
+                return false;
+            }
+            if(s[i]==')' && sr.top()!='('){
+                return false;
+            }
+            if(s[i]=='}' && sr.top()!='{'){
+                return false;
+            }
+            if(s[i]==']' && sr.top()!='['){
+                return false;
+            }
+            sr.pop();
+        }
+       }
+       return sr.empty();
+    }
+};
