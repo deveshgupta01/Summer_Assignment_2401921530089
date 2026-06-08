7e36132e91fdee1efc05d60ae15514396b61517d
deveshgupta01 <deveshg646@gmail.com>
2026-06-08 22:13:23 +0530

    Update First_unique_character_in_string.cpp

--- Week_2/Day_1/First_unique_character_in_string.cpp
+++ Week_2/Day_1/First_unique_character_in_string.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n=s.size();
+        vector<int>v(26,0);
+        // calculate frequency
+        for(int i=0;i<n;i++){
+            v[s[i]-'a']++;
+        }
+        // check frequency
+        for(int i=0;i<n;i++){
+            if(v[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
