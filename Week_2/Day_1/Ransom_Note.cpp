ff26e29724a612a9a085ed2b1c114944cad27af0
deveshgupta01 <deveshg646@gmail.com>
2026-06-08 22:19:12 +0530

    Update Ransom_Note.cpp

--- Week_2/Day_1/Ransom_Note.cpp
+++ Week_2/Day_1/Ransom_Note.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int m=ransomNote.size();
+        int n=magazine.size();
+        sort(ransomNote.begin(),ransomNote.end());
+        sort(magazine.begin(),magazine.end());
+        int i=0;
+        int j=0;
+        while(i<m && j<n){
+            if(ransomNote[i]==magazine[j]){
+                i++;
+                j++;
+            }
+            else if(ransomNote[i]>magazine[j]){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        return i==m;
+    }
+};
