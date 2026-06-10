cc8fed4433af888af2e7d9fe7e4260d52ddc3733
deveshgupta01 <deveshg646@gmail.com>
2026-06-10 23:23:19 +0530

    Update Repeated_Substring_Pattern.cpp

--- Week_2/Day_3/Repeated_Substring_Pattern.cpp
+++ Week_2/Day_3/Repeated_Substring_Pattern.cpp
@@ -1,1 +1,27 @@
 
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+
+        for(int len = 1; len <= n / 2; len++) {
+
+            if(n % len != 0) continue;
+
+            string pattern = s.substr(0, len);
+
+            bool ok = true;
+
+            for(int i = 0; i < n; i++) {
+                if(s[i] != pattern[i % len]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if(ok) return true;
+        }
+
+        return false;
+    }
+};
