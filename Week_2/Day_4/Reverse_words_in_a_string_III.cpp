6c8f2648217d9b58dded9047e23589db311da10a
deveshgupta01 <deveshg646@gmail.com>
2026-06-11 22:44:38 +0530

    Update Reverse_words_in_a_string_III.cpp

--- Week_2/Day_4/Reverse_words_in_a_string_III.cpp
+++ Week_2/Day_4/Reverse_words_in_a_string_III.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        int m=s.size();
+        int start=0;
+        for(int i=0;i<m;i++){
+            if(s[i]==' '){
+                reverse(s.begin()+start,s.begin()+i);
+                start=i+1;
+            }
+        }
+        reverse(s.begin()+start,s.end());
+        return s;
+    }
+};
